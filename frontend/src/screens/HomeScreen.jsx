5c8ab8ebc206247d683b2efd0de899a943396799
AlexMaderaP <alejandro.maderap@gmail.com>
2023-10-25 12:58:44 -0600

    Add fetch products from backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,20 @@
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Prod";
-import products from "../products";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async function () {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
