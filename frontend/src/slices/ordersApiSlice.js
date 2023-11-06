2cbc9cfc1ff98da65b34e9bb563af48c24ebef45
AlexMaderaP <alejandro.maderap@gmail.com>
2023-11-06 11:44:01 -0600

    Add Order list screen for admin

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -35,6 +35,12 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    getOrdersList: builder.query({
+      query: () => ({
+        url: ORDERS_URL,
+      }),
+      keepUnusedDataFor: 5,
+    }),
   }),
 });
 
@@ -44,4 +50,5 @@ export const {
   usePayOrderMutation,
   useGetPayPalClientIdQuery,
   useGetMyOrdersQuery,
+  useGetOrdersListQuery,
 } = ordersApiSlice;
